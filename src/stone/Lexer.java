63b929d6bebe1dc44538226ac78b43db9aca7fe1
nicklausliu <nicklausliu@tencent.com>
2022-06-27 11:16:06 +0800

    feat: test login token

--- src/stone/Lexer.java
+++ src/stone/Lexer.java
@@ -6,4 +6,5 @@ public class Lexer {
     public static final String regexPat = "\\s*((//.*)|([0-9]+)|(\"(\\\\\"|\\\\\\\\|\\\\n|[^\"])*\")"
             + "|[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\\|\\||\\p{Punct})?";
     private Pattern pattern = Pattern.compile(regexPat);
+
 }
