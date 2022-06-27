db020e3831b542515034675df4da8c7810efda22
nicklausliu <nicklausliu@tencent.com>
2022-06-27 11:08:34 +0800

    feat: add jetbrains configuration

---
+++ src/stone/Lexer.java
@@ -0,0 +1,9 @@
+package stone;
+
+import java.util.regex.Pattern;
+
+public class Lexer {
+    public static final String regexPat = "\\s*((//.*)|([0-9]+)|(\"(\\\\\"|\\\\\\\\|\\\\n|[^\"])*\")"
+            + "|[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\\|\\||\\p{Punct})?";
+    private Pattern pattern = Pattern.compile(regexPat);
+}
