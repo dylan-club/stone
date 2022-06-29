24ee296c65523b045111ac20210c64570eb71325
nicklausliu <nicklausliu@tencent.com>
2022-06-29 15:58:30 +0800

    feat: add ParseException

--- src/stone/Lexer.java
+++ src/stone/Lexer.java
@@ -1,10 +1,20 @@
 package stone;
 
+import java.io.LineNumberReader;
+import java.io.Reader;
+import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Lexer {
     public static final String regexPat = "\\s*((//.*)|([0-9]+)|(\"(\\\\\"|\\\\\\\\|\\\\n|[^\"])*\")"
             + "|[A-Z_a-z][A-Z_a-z0-9]*|==|<=|>=|&&|\\|\\||\\p{Punct})?";
     private Pattern pattern = Pattern.compile(regexPat);
+    private ArrayList<Token> queue = new ArrayList<>();
+    private boolean hasMore;
+    private LineNumberReader reader;
 
+    public Lexer(Reader r) {
+        hasMore = true;
+        reader = new LineNumberReader(r);
+    }
 }
